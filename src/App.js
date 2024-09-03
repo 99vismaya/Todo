ea91fd5232c132a3955682ac24b3d61ee2c88b3f
99vismaya <mkvismaya99@gmail.com>
2024-09-03 17:18:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import './App.css';
-import Signup from './components/Signup';
-import Login from './components/Login';
 import Todo from './components/Todo';
-import './style.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import './style.css'
 
 
 function App() {
   return (
-    <Router>
-    <>
-    <Routes>
-    <Route path="/" element={<Signup/>}>
-    </Route>
-    <Route path="/Login" element = {<Login/>}>
-    </Route>
-    <Route path="/Todo" element = {<Todo/>}>
-    </Route>
-    </Routes>
-    </>
-    </Router>    
+    <Todo/>
   );
 }
 
